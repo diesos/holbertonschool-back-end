76cb28a78f4679bc4ce043c4ea1feec4db7a7806
Diesos <ozturk@live.Fr>
2024-04-16 16:39:20 +0200

    Add script for exporting data to JSON format in api/2-export_to_JSON.py

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Script that exports data in the JSON format """
+
+import json
+import requests
+import sys
+
+API_URL = "https://jsonplaceholder.typicode.com/"
+
+if __name__ == "__main__":
+
+	employee_id = sys.argv[1]
+
+	employee = requests.get(API_URL + "users/{}".format(employee_id)).json()
+
+	todo_list = requests.get("{}todos?userId={}".format(API_URL, employee_id)).json()
+
+	with open("{}.json".format(employee_id), "w") as json_file:
+		json.dump(
+			{
+				employee_id: [
+					{
+						"task": task.get("title"),
+						"completed": task.get("completed"),
+						"username": employee.get("username"),
+					}
+					for task in todo_list
+				]
+			},
+			json_file,
+		)
